594cf3e8dd9b13e219cf15ddc6c252ef3e886579
yu <email@yuyuyuyuyu.dev>
2025-07-05 15:31:33 +0900

    build(plugin): upgrade dependencies

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "2.2.0"
     id("com.gradle.plugin-publish") version "1.3.1"
     `java-gradle-plugin`
-    id("com.vanniktech.maven.publish") version "0.31.0"
+    id("com.vanniktech.maven.publish") version "0.33.0"
     `kotlin-dsl`
     id("com.github.node-gradle.node") version "7.1.0"
 }
@@ -36,7 +36,7 @@ dependencies {
     implementation(gradleApi())
     implementation(localGroovy())
     implementation("com.github.node-gradle.node:com.github.node-gradle.node.gradle.plugin:7.1.0")
-    implementation("org.jsoup:jsoup:1.19.1")
+    implementation("org.jsoup:jsoup:1.21.1")
 }
 
 mavenPublishing {
